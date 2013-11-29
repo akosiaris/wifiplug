b1840de9037cf50e1ce6e6d37cddfc499e94cb09
Alexandros Kosiaris <akosiaris@gmail.com>
2013-11-29 14:11:05 +0200

    Adding features
    Parsing JSON responses
    Extracting session key
    Using wireshark prefs for master key

--- wifiplug.lua
+++ wifiplug.lua
@@ -1,10 +1,12 @@
 -- wifiplug android app protocol
-require("openssl")
--- Yes this is the master 3DES key used to authenticate users and negotiate 3DES session keys. Secure heh ?
-key='""OX'..string.char(0x88)..'8(%%yQ'..string.char(0xcb)..'0@6(3)'..string.char(0x11)..'KD'..string.char(0xfe)..'vh'
+openssl = require("openssl")
+json = require("dkjson")
+sessionkey = ''
 des3ecb = openssl.get_cipher('des-ede3')
 -- declare out protocol
 wifiplug_proto = Proto("wifiplug", "wifiplug android app protocol")
+-- Yes this is the master 3DES key used to authenticate users and negotiate 3DES session keys. Secure heh ?
+wifiplug_proto.prefs.masterkey = Pref.string('masterkey', '""OX'..string.char(0x88)..'8(%%yQ'..string.char(0xcb)..'0@6(3)'..string.char(0x11)..'KD'..string.char(0xfe)..'vh', 'Wifiplugs master key')
 -- helper functions
 -- Compatibility: Lua-5.1
 function split(str, pat)
@@ -53,38 +55,113 @@ function decrypt(c, k)
 	return p
 end
 
-
 function wifiplug_proto.dissector(buffer, pinfo, tree)
 	pinfo.cols.protocol = "WIFIPLUG"
 	local subtree = tree:add(wifiplug_proto, buffer(), "Wifiplug Protocol Data")
-	local plaintext = decrypt(buffer():string(), key)
-	print(buffer():string())
-	print(plaintext)
-	local parts = split(plaintext, ",")
-	for i,v in pairs(parts) do
-		v = string.gsub(v, 'E+$', '')
-		subtree:add(buffer(), detect(i, v))
+
+	local key
+	local plaintext
+	if sessionkey ~= ''  then
+		key = sessionkey
+	else
+		key = wifiplug_proto.prefs.masterkey
+	end
+
+	-- First let's try with a possible key
+	plaintext = decrypt(buffer():string(), key)
+	-- Trying to decrypt the first packets in a conversation? Fallback to masterkey
+	if not detect_correct_decryption(plaintext) then
+		plaintext = decrypt(buffer():string(), wifiplug_proto.prefs.masterkey)
+	end
+	local sk = extract_session_key(plaintext)
+	if sk ~= nil then
+		sessionkey = sk
+	end
+	populate_tree(plaintext, subtree, buffer)
+end
+
+function detect_correct_decryption(s)
+	if string.match(s, '^B+') then
+		return true
+	end
+	return false
+end
+
+function extract_session_key(s)
+	if string.match(s, '^BBBB%+OK') then
+		local parts = split(s, " ")
+		k = parts[#parts]
+		k = string.gsub(k, 'E+$', '')
+		k = string.fromhex(k)
+		return k
+	else
+		return nil
+	end
+end
+
+-- Populate tree
+function populate_tree(s, t, b)
+-- Let's see if it is json first
+	local obj, pos, err = json.decode(string.gsub(s, '^BBBB({.*})EEEE$', "%1"))
+	if not err then
+		maclist = t:add(b(), 'MAC list')
+		for i,mac in pairs(obj.macList) do
+			mact = maclist:add(b(), 'MAC')
+			mact:add(b(), 'Id:'             ..mac.Id)
+			mact:add(b(), 'Mac Address:'        ..mac.MacAddr)
+			mact:add(b(), 'Major Type:'      ..mac.MajorType)
+			mact:add(b(), 'Minor Type:'      ..mac.MinorType)
+			mact:add(b(), 'Name:'           ..mac.Name)
+			mact:add(b(), 'Remark:'         ..mac.Remark)
+			mact:add(b(), 'Status:'         ..tostring(mac.Status))
+			mact:add(b(), 'Switcher:'       ..mac.Switcher)
+			mact:add(b(), 'Switcher Count:'  ..mac.SwitcherCount)
+			mact:add(b(), 'SwitcherName:'   ..mac.SwitcherName)
+			mact:add(b(), 'Switcher Value1:' ..mac.SwitcherValue1)
+			mact:add(b(), 'Switcher Value2:' ..mac.SwitcherValue2)
+			mact:add(b(), 'Switcher Value3:' ..mac.SwitcherValue3)
+			mact:add(b(), 'Switcher Value4:' ..mac.SwitcherValue4)
+			mact:add(b(), 'Switcher Value5:' ..mac.SwitcherValue5)
+			mact:add(b(), 'Update Time:'     ..mac.UpdateTime)
+		end
+		timerlist = t:add(b(), 'Timer list')
+		for i,timer in pairs(obj.timerList) do
+			timert = timerlist:add(b(), 'Timer')
+			timert:add(b(), 'Id:'         ..timer.Id)
+			timert:add(b(), 'MAC Id:'      ..timer.MacId)
+			timert:add(b(), 'One Switcher:'..timer.OneSwitcher)
+			timert:add(b(), 'Remark:'     ..timer.Remark)
+			timert:add(b(), 'Switcher Num:'..timer.SwitcherNum)
+			timert:add(b(), 'Timer:'      ..timer.Timer)
+			timert:add(b(), 'Timer Type:'  ..timer.TimerType)
+		end
+	else
+		local parts = split(s, ",")
+		for i,v in pairs(parts) do
+			v = string.gsub(v, 'E+$', '')
+			st = t:add(b(), 'Unidentified yet data')
+			detect(i, v, st, b)
+		end
 	end
 end
 
 -- Detect parts of the packet
-function detect(i, v)
+function detect(i, v, subtree, b)
 	if v == 'BBBB1' then
-		return "Command: Login"
+		subtree:set_generated()
+		subtree:set_text('Command: Login')
 	elseif i == 2 then
-		return "Username: " .. v
+		subtree:set_text("Username: " .. v)
 	elseif i == 3 then
-		return "Password(MD5): " .. v
+		subtree:set_text("Password(MD5): " .. v)
 	elseif i == 4 then
-		return "Date: " .. v
+		subtree:set_text("Date: " .. v)
 	elseif i == 5 then
-		return "App_ID: " .. v
+		subtree:set_text("App_ID: " .. v)
 	elseif i == 6 then
-		return "Timezone Offset?: " .. v
+		subtree:set_text("Timezone Offset?: " .. v)
 	elseif i == 7 then
-		return "Version: " .. v
-	else
-		return "DATA: " .. v
+		subtree:set_text("Version: " .. v)
 	end
 end
 
