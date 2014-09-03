aa367678789cfe5c85bec0d2595b749245f7ca50
Alexandros Kosiaris <akosiaris@gmail.com>
2014-09-03 19:57:30 +0300

    Have JSON displayed nicely
    Just ident it
    Also lint by using single quotes and not double quotes

--- wifiplug2.lua
+++ wifiplug2.lua
@@ -1,10 +1,10 @@
 -- wifiplug android app protocol
 require('wifiplug_common')
 -- declare out protocol
-wifiplug_proto = Proto("wifiplug2", "wifiplug2 android app protocol")
+wifiplug_proto = Proto('wifiplug2', 'wifiplug2 android app protocol')
 function wifiplug_proto.dissector(buffer, pinfo, tree)
-	pinfo.cols.protocol = "WIFIPLUG2"
-	local subtree = tree:add(wifiplug_proto, buffer(), "Wifiplug2 Protocol Data")
+	pinfo.cols.protocol = 'WIFIPLUG2'
+	local subtree = tree:add(wifiplug_proto, buffer(), 'Wifiplug2 Protocol Data')
 	local calc_checksum = getchecksum(buffer())
 	local packet_checksum = buffer(9,4)
 	if calc_checksum == packet_checksum:string() then
@@ -14,20 +14,24 @@ function wifiplug_proto.dissector(buffer, pinfo, tree)
 	end
 	local data = zlib.inflate(buffer(14):string()):read('*l')
 
-	subtree:add(buffer(), "Data size: " .. buffer():len()):set_generated()
-	subtree:add(buffer(0,2), "Header (constant): " .. buffer(0,2):uint())
-	subtree:add(buffer(2,1), "Protocol Version (constant ?): " .. buffer(2,1):uint())
-	subtree:add(buffer(3,1), "Size: " .. buffer(3,1):uint())
-	subtree:add(buffer(4,3), "Size (wtf?): " .. buffer(4,3):uint())
-	subtree:add(buffer(7,1), "Sequence Number (1/2): " .. buffer(7,1):uint())
-	subtree:add(buffer(8,1), "Sequence Number (2/2): " .. buffer(8,1):uint())
-	subtree:add(buffer(9,4), "Checksum: " .. packet_checksum:string():tohex() .. checksum)
-	subtree:add(buffer(13,1), "Command Byte: " .. buffer(13,1):string():tohex())
-	subtree:add(buffer(14), "Compressed Data: " .. buffer(14):string():tohex())
-	subtree:add(buffer(14), "Uncompress Data: " .. data):set_generated()
+	subtree:add(buffer(), 'Data size: ' .. buffer():len()):set_generated()
+	subtree:add(buffer(0,2), 'Header (constant): ' .. buffer(0,2):uint())
+	subtree:add(buffer(2,1), 'Protocol Version (constant ?): ' .. buffer(2,1):uint())
+	subtree:add(buffer(3,1), 'Size: ' .. buffer(3,1):uint())
+	subtree:add(buffer(4,3), 'Size (wtf?): ' .. buffer(4,3):uint())
+	subtree:add(buffer(7,1), 'Sequence Number (1/2): ' .. buffer(7,1):uint())
+	subtree:add(buffer(8,1), 'Sequence Number (2/2): ' .. buffer(8,1):uint())
+	subtree:add(buffer(9,4), 'Checksum: ' .. packet_checksum:string():tohex() .. checksum)
+	subtree:add(buffer(13,1), 'Command Byte: ' .. buffer(13,1):string():tohex())
+	subtree:add(buffer(14), 'Compressed Data: ' .. buffer(14):string():tohex())
+	local obj, pos, err = json.decode(data)
+	if not err then
+		data = json.encode(obj, {['indent'] = true})
+	end
+	subtree:add(buffer(14), 'Uncompressed Data: ' .. data):set_generated()
 
 	-- populate_tree(plaintext, subtree, buffer)
 end
 
-tcp_table = DissectorTable.get("tcp.port")
+tcp_table = DissectorTable.get('tcp.port')
 tcp_table:add(227, wifiplug_proto)
