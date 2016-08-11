akosiaris/wifiplug

commit 95768ffa2f3fd894c2b1b9c739b31d349aa61296

Force a quit if too many errors appear
When too many errors appear in the V1 handling code, something's not
right. Force a return and rely on the caller to restart us (like
systemd/upstart/supervisord)

client.lua
@@ -24,6 +24,9 @@ local log_level = config.log_level
 local known_macs = {}
 local scheduled_macs = {}
 local icaldata = nil
+
+local errorcounter = 0
+local max_errors = 100
 scheduler_timer = 30 --seconds
 
 openssl = require('openssl')
@@ -673,6 +676,10 @@ while true do
                 status,err = client:receive('*l')
                 if not status then
                     log.error(err)
+                    errorcounter = errorcounter + 1
+                    if errorcounter = maxerrors then
+                        return  -- We got too many errors, just quit
+                    end
                     break
                 end
                 status = decrypt(status, session_key)

commit f38b9b33e073da67f9d9f3e5f52c64a78a77521f

Fix typo introduced in 95768ff

client.lua
@@ -677,7 +677,7 @@ while true do
                 if not status then
                     log.error(err)
                     errorcounter = errorcounter + 1
-                    if errorcounter = maxerrors then
+                    if errorcounter >= maxerrors then
                         return  -- We got too many errors, just quit
                     end
                     break